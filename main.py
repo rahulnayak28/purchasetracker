6727e7fc1b324fab726dded8683120a4b53feeed
rahulnayak28 <rahulnayak28@gmail.com>
2023-05-20 19:54:09 +0530

    adding PyQT5 libraries

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+"""
+Purchase Tracker App to track purchases.
+"""
+#import libraries
+from PyQt5.QtWidgets import (
+    QGroupBox,
+    QLabel,
+    QLineEdit,
+    QPlainTextEdit,
+    QRadioButton,
+    QSpinBox,
+    QVBoxLayout,
+    QWidget,
+    QApplication,
+    QFormLayout,
+)
 
+#end import libraries
